3886b81adb583241cbc638acfe841da2085654d4
Pierito Rolando <rolandopiero63@gmail.com>
2021-02-21 09:35:00 -0500

    momentos antes de cagarme la navbar

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,54 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import 'tailwindcss/tailwind.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Inicio | Piero Rolando</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css" integrity="sha256-x8PYmLKD83R9T/sYmJn1j3is/chhJdySyhet/JuHnfY=" crossorigin="anonymous" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"></link>
+    <nav className="flex items-center bg-gray-100 p-2 flex-wrap  w-full justify-between px-5 lg:px-20 dark:bg-gray-500">
+      <a href="#" className="p-2 mr-4 inline-flex items-center">
+        <span className="text-2xl text-black dark:text-white  tracking-wide arciform pb-1">pr</span>
+      </a>
+      <span className="material-icons dark:text-white">
+        nightlight_round
+      </span>
+      <button className="text-white inline-flex p-3 hover:bg-gray-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler" data-target="#navigation">
+        <i className="material-icons">menu</i>
+      </button>
+      <div className="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto" id="navigation">
+        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="#" className="lg:inline-flex lg:w-auto w-full px-4 py-2 hover:shadow-xl rounded-3xl text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+            <span>Home</span>
+          </a>
+          <a href="#" className="lg:inline-flex lg:w-auto w-full px-4 py-2 hover:shadow-xl rounded-3xl text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+            <span>About</span>
+          </a>
+          <a href="#" className="lg:inline-flex lg:w-auto w-full px-4 py-2 hover:shadow-xl rounded-3xl text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+            <span>Services</span>
+          </a>
+          <a href="#" className="lg:inline-flex lg:w-auto w-full px-4 py-2 hover:shadow-xl rounded-3xl text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+            <span>Gallery</span>
+          </a>
+          <a href="#" className="lg:inline-flex lg:w-auto w-full px-4 py-2 hover:shadow-xl rounded-3xl text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+            <span>Products</span>
+          </a>
+          <a href="#" className="lg:inline-flex lg:w-auto w-full px-4 py-2 hover:shadow-xl rounded-3xl text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+            <span>Contact Us</span>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
+      </div>
+    </nav>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+
+</>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  purge: [], 
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      textOpacity: ['dark']
+    }
+  },
+  plugins: [],
+}
