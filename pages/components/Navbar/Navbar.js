3f146b45b916fd7fe126dc259c32c1e852c870e9
Pierito Rolando <rolandopiero63@gmail.com>
2021-03-09 15:26:07 -0500

    irrelevant modification

--- pages/components/Navbar/Navbar.js
+++ pages/components/Navbar/Navbar.js
@@ -50,7 +50,7 @@ export default function Navbar() {
               From: "opacity-100 translate-y-0"
               To: "opacity-0 translate-y-1" 
           --> */}
-          <SubMenu active={ proyectsnav }  />
+          <SubMenu active={ proyectsnav } setProyectsnav={setProyectsnav} />
         </div>
 
         <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-400">Blog</a>
@@ -151,9 +151,9 @@ export default function Navbar() {
 
 
 
-export const SubMenu = ({ active }) => {
+export const SubMenu = ({ active, setProyectsnav }) => {
   return (
-    <div className={`${ active ? 'hidden' : '' } absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2`}>
+    <div onMouseLeave={ () => { setProyectsnav(true) } } className={`${ active ? 'hidden' : '' } absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2`}>
     <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
       <div className="relative grid gap-6 bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
         <a href="#" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-700">
