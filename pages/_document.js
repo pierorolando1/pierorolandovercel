d74753677e9f5a12092f389636e4bdb8a5c86e63
Pierito Rolando <rolandopiero63@gmail.com>
2021-03-14 08:32:08 -0500

    moments before to make a refaroctisation

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
           />
+          <link 
+            href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" 
+            rel="stylesheet" />
         </Head>
         <body>
           <Main />
