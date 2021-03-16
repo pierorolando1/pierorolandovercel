934b9f58fb7008d648d1c84f054a6ae557841be9
Pierito Rolando <rolandopiero63@gmail.com>
2021-03-15 19:50:57 -0500

    add the seo library

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,17 @@ import { WelcomePage } from '../components/WelcomePage'
 import Navbar from '../components/Navbar/Navbar'
 import React, { useEffect } from 'react'
 import { NormalPage } from '../components/NormalPage'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
 
   return (
+    
     <>
+        <NextSeo
+      title="Inicio | Piero Rolando"
+      description="Portafolio and personal blog of Piero Rolando"
+    />
       <NormalPage  title="Inicio">
         <WelcomePage />
       </NormalPage>
