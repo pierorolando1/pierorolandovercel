641176c6a652af6dcd8273dea3eee3dce4648189
Pierito Rolando <rolandopiero63@gmail.com>
2021-04-03 15:10:13 -0500

    add typescript to proyectos

--- pages/proyectos.tsx
+++ pages/proyectos.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NormalPage } from '../components/NormalPage'
 const Proyectos = () => {
     return (
-        <NormalPage>
+        <NormalPage title="Proyectos">
             <div>Proyectos</div>
         </NormalPage>
     )
