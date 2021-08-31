d9c12efdeee560adb00c54d921467bd0b13522a3
Piero Rolando <rolandopiero63@gmail.com>
2021-08-31 03:19:22 -0700

    correct the url bug

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -8,33 +8,34 @@ import { motion } from 'framer-motion'
 const Post = () => {
 
     const router = useRouter()
-    const { id } = router.query
-    console.log(id)
     const [post, setPost] = useState<any>({})
-
+    
     useEffect(() => {
         (async () => {
+            const id = window.location.pathname.split("blog/")[1]
+            //const { id } = router.query
+            console.log(id)
             const data = await db.collection("posts").doc(`${id}`).get()
             const post = data.data()
             setPost(post)
-            document.getElementById("container").innerHTML = post.content
+            document.getElementById("container").innerHTML = post?.content
         })()
 
 
     }, [])
 
     return (
-        <NormalPage footer={false} title={id}>
+        <NormalPage footer={false} title={post?.title}>
             <div className="bg-gray-900 w-full min-h-screen">
                 <div className="top-0 fixed image-container w-full">
                     <div className="relative w-full h-full ">
                         <div className="w-full h-full absolute bg-gray-900 bg-opacity-80" />
                         <div className="absolute w-full h-full">
-                            <h1 className="w-full h-full flex items-end p-4 md:pb-10 pb-4 md:text-5xl sm:text-3xl text-2xl font-bold text-gray-200 max-w-8xl mx-auto">{post.title}</h1>
+                            <h1 className="w-full h-full flex items-end p-4 md:pb-10 pb-4 md:text-5xl sm:text-3xl text-2xl font-bold text-gray-200 max-w-8xl mx-auto">{post?.title}</h1>
                         </div>
                         <motion.img initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }} src={post.imagen} className="h-full w-full object-cover z-0 transition-opacity" />
+                            exit={{ opacity: 0 }} src={post?.imagen} className="h-full w-full object-cover z-0 transition-opacity" />
                     </div>
                 </div>
                 <div className="bg-rd-600 w-full min-h-screen div-margin-top z-20 relative bg-gray-900 md:py-10 py-6">
